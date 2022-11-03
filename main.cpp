00d6fbbd39eeceba394e0d6b4ce80f35ebe07d67
Gdappl <90865294+gdapplpsycho100@users.noreply.github.com>
2022-11-03 13:24:24 -0700

    Even Sum & Odd Sum

---
+++ main.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int sums(vector<int> numbers) {
+  int evenSum = 0;
+  int oddSum = 0;
+  if (numbers.size() == 0) {
+    return 0;
+  }
+  if (numbers.size() == 1) {
+    return numbers[0];
+  }
+  for (int i = 0; i < numbers.size(); i++) {
+    if (i % 2 == 0) {
+      if (-100 <= numbers[i] && numbers[i] <= 100) {
+        evenSum += numbers[i];
+      }
+    } else if (i % 2 != 0) {
+      if (-100 <= numbers[i] && numbers[i] <= 100) {
+        oddSum += numbers[i];
+      }
+    }
+  }
+  cout << evenSum << endl;
+  cout << oddSum << endl;
+  return evenSum - oddSum;
+}
+
+int main(int argc, char *argv[]) {
+  vector<int> num1{101, 3, 4, 359, 2, 5};
+  cout << sums(num1) << endl;
+  vector<int> num2{-2, 234, 100, 99, 540, -1};
+  cout << sums(num2) << endl;
+  vector<int> num3{-9};
+  cout << sums(num3) << endl;
+}
