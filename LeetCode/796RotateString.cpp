53053623780fa1debb48a4f0865f59e944bccb24
Gdappl <90865294+gdapplpsycho100@users.noreply.github.com>
2022-11-06 11:18:37 -0800

    Update 796RotateString.cpp

--- LeetCode/796RotateString.cpp
+++ LeetCode/796RotateString.cpp
@@ -1,1 +1,37 @@
+#include <iostream>
+#include <stack>
+#include <string>
+#include <utility>
+#include <vector>
 
+using namespace std;
+
+string rotate(string s) {
+  char back = s[0];
+  s.erase(s.begin());
+  s += back;
+  return s;
+}
+
+bool rotateString(string s, string goal) {
+  if (s.size() != goal.size()) {
+    return false;
+  }
+  if (s == goal) {
+    return true;
+  }
+  int n = s.size();
+  for (int i = 0; i < s.size(); i++) {
+    s = rotate(s);
+    if (s == goal) {
+      return true;
+    }
+  }
+  return false;
+}
+
+int main(int argc, char *argv[]) {
+  string s = "abcde";
+  string goal = "cdeab";
+  cout << rotateString(s, goal) << endl;
+}
